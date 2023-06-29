8773d386ec4742c936c4fbac6849d30bbb356353
Dinakaran Kannadhasan <dinakaran.kannadhasan@gmail.com>
2023-06-29 19:24:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
-import React, { } from 'react';
+import React, { useState, useEffect } from 'react';
+import { MarketplaceMetering, MarketplaceEntitlementService } from 'aws-sdk';
 
 const App = () => {
+  const [customerInfo, setCustomerInfo] = useState(null);
+
+  useEffect(() => {
+    const getCustomerInfo = async () => {
+      try {
+        const marketplaceMetering = new MarketplaceMetering();
+        const marketplaceEntitlementService = new MarketplaceEntitlementService();
+
+        // Resolve customer using the marketplace token
+        const resolveCustomerParams = {
+          RegistrationToken: 'YOUR_MARKETPLACE_TOKEN',
+        };
+        const resolveCustomerResponse = await marketplaceMetering
+          .resolveCustomer(resolveCustomerParams)
+          .promise();
+
+        const customerId = resolveCustomerResponse.CustomerIdentifier;
+
+        // Retrieve customer information using Marketplace Metering API
+        const meteringParams = {
+          // Provide necessary parameters for metering API, including 'ProductCode' and 'Timestamp'
+          CustomerIdentifier: customerId,
+        };
+        const meteringResponse = await marketplaceMetering
+          .meterUsage(meteringParams)
+          .promise();
+
+        // Retrieve customer entitlement information using Marketplace Entitlement API
+        const entitlementParams = {
+          // Provide necessary parameters for entitlement API
+          CustomerIdentifier: [ customerId ],
+        };
+        const entitlementResponse = await marketplaceEntitlementService
+          .listEntitlements(entitlementParams)
+          .promise();
+
+        // Set the customer information in the state
+        setCustomerInfo({ metering: meteringResponse, entitlement: entitlementResponse });
+      } catch (error) {
+        console.error('Error retrieving customer information:', error);
+      }
+    };
+
+    getCustomerInfo();
+  }, []);
+
+  if (!customerInfo) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h1>React App!</h1>
+      <h2>Customer Information</h2>
+      {/* Display customer information */}
     </div>
   );
-}
+};
 
 export default App;
